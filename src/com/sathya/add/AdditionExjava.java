697e19cd7d50f7e539440748270f3e74f073e1da
kisugallaprashanthi <kisugallaprashanthi12@gmail.com>
2023-05-31 14:55:33 +0530

    Addition of two numbers

---
+++ src/com/sathya/add/AdditionExjava.java
@@ -0,0 +1,14 @@
+package com.sathya.add;
+
+public class AdditionExjava {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		int res=num1+num2;
+		System.out.println(res);
+		
+
+	}
+
+}
